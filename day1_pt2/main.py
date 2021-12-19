cb3a6d494a9902f09779b2e9b2fb9abf4bafef79
EchoAlice <echoalice0@gmail.com>
2021-12-19 00:26:59 -0500

    Update to Day 1 pt 2! :wq

--- day1_pt2/main.py
+++ day1_pt2/main.py
@@ -3,41 +3,28 @@ if __name__ == "__main__":
     #Read text file into numbers within an array
     with open("input_test.txt") as values:
         depths = values.readlines()
-
-    previous_element = 0
-    depth_increases = 0
-
-    current_array = []*4
-    next_array = []*4
-
-    # Cycles through every value in input.txt
-    for depth in depths:
-        # How do I get depths to cycle through four elements, instead of copying the same one over and over        
-        for i in range(4):
-            if i < 3:
-                # I want to print out value at depth + i
-                print(depths[i])
-                print(depths[i+1])
-                print(depths[i+2])
-                #current_array[i] = int (depth)
-                #print("Current array: " + current_array[i])
-            if i > 0:
-                print(depths[i])
-                print(depths[i+1])
-                print(depths[i+2])
-                #next_array[i] = int (depth)
-                #print("Next array: " + str(next_array[i]))
-
-
-    # At end of cycle, turn current array to previous array and rerun the loop.   Recursion???
             
+    previous_int = 0
+    current_int = 0
+    
+    #Change depths to ints
+    for i in range(len(depths)):
+        current_int = int (depths[i] + depths[i + 1] + depths[i + 2])
+        print(current_int)
 
+        if i > 0:
+            previous_int = int (depths[i - 1] + depths[i] + depths[i + 1])
+            print(previous_int)
         
-        
-        
+      
+    
+
+
+
+ # Day 1 part 1 code.  Useful info       
         
             
-   # depth_calculation = int(depth) - previous_element
+   #depth_calculation = int(depth) - previous_element
     #if depth_calculation > 0 and previous_element != 0:
         #decreasing from the previous element
     
