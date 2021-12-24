cbd8e96ea99b22da13c140406933fcfc2b4ce838
EchoAlice <echoalice0@gmail.com>
2021-12-23 22:02:45 -0500

    Day 3 part1.py

--- day3/part1.py
+++ day3/part1.py
@@ -1,42 +1,37 @@
 if __name__ == "__main__":
     
     # Read text file into numbers within an array
-    with open("day3/input_test.txt") as values:
-        depths = values.readlines()
+    with open("day3/input.txt") as values:
+         binary_input = values.readlines()
 
-    # Why is len(depths[0]) = 6?
-    col = len(depths[0]) - 1   
-    row = len(depths)
-
-    # Set empty array for values of each column
-    count_0 = [0] * col
-    count_1 = [0] * col
-    gamma_rate = ""
-    for i in range(row):
-        string = depths[i]
-        print("Row: " + str(i + 1))
-        print("String: " + string)
-        # Find most common bit in gamma rate
-        # Access each individual value in list.
-        for j in range(col):
-            if int(string[j]) == 0:
-                count_0[i] = count_0[i] + 1 
-            if int(string[j]) == 1:
-                count_1[i] = count_1[i] + 1
-        print("Count_0 ++: " + str(count_0[i]))
-        print("Count_1 ++: " + str(count_1[i]))
-
-        # How do I add 1 or 0 to a string to create a binary number?
-        #if count_0 > count_1:
-        #    gamma_rate += str(0)
-        #if count_0 < count_1:
-        #    gamma_rate += str(1)
-        #print("Gamma Rate: " + gamma_rate) 
+    # Why is len(binary_input[0]) = 6?
+    col = len(binary_input[0]) - 1   
+    row = len(binary_input)
+     
+    binary_input_gamma = ""
+    binary_input_epsilon = ""
     for i in range(col):
-        # How do I add 1 or 0 to a string to create a binary number?
-        if count_0[i] > count_1[i]:
-            gamma_rate += str(0)
-            print("Gamma Rate: " + gamma_rate)
-        if count_0[i] < count_1[i]:
-            gamma_rate += str(1)
-            print("Gamma Rate: " + gamma_rate)    
+        #print("Column: " + str(i + 1))
+        # Find most common bit in gamma rate                               # Why did changing binary_input[i][j] to binary_input[j][i] work?
+        # Access each individual value in list.                            # [5][12]
+        count_0 = 0
+        count_1 = 0 
+        for j in range(row): 
+            # Calculates the number of times first bit of each number showed up
+            if int(binary_input[j][i]) == 0:                                   
+                count_0 = count_0 + 1 
+            if int(binary_input[j][i]) == 1:                
+                count_1 = count_1 + 1
+        # When ith digit in binary_input is a 0
+        if count_0 > count_1:
+            binary_input_gamma += str(0)
+            binary_input_epsilon += str(1)
+        # When ith digit in binary_input is a 1
+        if count_0 < count_1:
+            binary_input_gamma += str(1)
+            binary_input_epsilon += str(0)
+    #Convert
+    gamma_rate = int(binary_input_gamma, 2)
+    epsilon_rate =  int(binary_input_epsilon, 2)
+    power_consumption = gamma_rate*epsilon_rate
+    print(str(power_consumption)) 
